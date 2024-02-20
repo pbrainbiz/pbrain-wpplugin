55b3db42293a79539cb2d7de381e913f60ad1f34
xiao0027 <xiao.yuhong.86@gmail.com>
2024-02-20 17:37:09 +0800

    initial

---
+++ pbrain-wpplugin.php
@@ -0,0 +1,12 @@
+/*
+Plugin Name:  WPBeginner Plugin Tutorial
+Plugin URI:   https://www.pbrain.biz
+Description:  Lead Generation Custom ChatGPT
+Version:      1.0
+Author:       PBrain
+Author URI:   https://www.pbrain.biz
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  wpb-tutorial
+Domain Path:  /languages
+*/
